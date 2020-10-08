c85842e366ee5cc0c84d0add219c59893bad3e8f
Charles Pierce <cpierce.grad@gmail.com>
2020-10-08 00:44:18 -0700

    Add 'list' subcommand to 'stocks favorite'

---
+++ src/commands/favorite/list.js
@@ -0,0 +1,6 @@
+exports.command = 'list';
+exports.desc = 'Show the list of your favorite stocks';
+exports.builder = {};
+exports.handler = function (argv) {
+    console.log('Running favorite list');
+};
