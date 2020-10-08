bf8f68b2fa12ee2b9055c9e3069d1b3c6e3a2216
Charles Pierce <cpierce.grad@gmail.com>
2020-10-08 12:29:14 -0700

    Add spinner when fetching data

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const { StocksError } = require('./error');
 const { getToken } = require('./store');
 const fetch = require('node-fetch');
+const { Spinner } = require('cli-spinner');
 
 module.exports = {
     getPrices,
@@ -9,15 +10,18 @@ module.exports = {
 const priceFormatter = Intl.NumberFormat(undefined, { style: 'currency', currency: 'USD' });
 const percentFormatter = Intl.NumberFormat(undefined, { style: 'percent', minimumFractionDigits: 2 });
 
-// TODO: Show spinner when fetching data
 async function getPrices(symbols) {
+    const spinner = new Spinner('Fetching stock price data');
     try {
+        spinner.start();
         let res = await fetch(buildPriceUrl(symbols));
         let json = await res.json();
 
         return Object.values(json).map(data => parseQuote(data.quote));
     } catch {
         throw new StocksError('Unable to fetch stock data', 'Please verify your internet connection and confirm the stock symbols are correct');
+    } finally {
+        spinner.stop(true);
     }
 }
 
